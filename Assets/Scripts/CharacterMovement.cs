50a9f255941f58b72a2cdef2c04a5edf6cdedd17
Heasds <68012931+FynjyDev@users.noreply.github.com>
2023-04-02 19:07:33 +0300

    CharacterMovement logic and visual

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    //public Animator characterAnimator;
+    public Transform characterTr;
+    public Rigidbody characterBody;
+    //public Joystick joystick;
+
+    public float moveSpeed;
+    public float rotateSpeed;
+
+    private Vector3 _InputVector;
+
+    private void FixedUpdate()
+    {
+        CharacterMove();
+        CharacterRotate();
+    }
+
+    private void CharacterMove()
+    {
+        //if (SystemInfo.deviceType == DeviceType.Handheld)
+        //{
+            //_InputVector.x = joystick.Horizontal;
+            //_InputVector.z = joystick.Vertical;
+        //}
+        //else
+        //{
+            _InputVector.x = Input.GetAxis("Horizontal");
+            _InputVector.z = Input.GetAxis("Vertical");
+        //}
+
+        characterBody.velocity = _InputVector * moveSpeed;
+        //characterAnimator.SetBool("IsRun", _InputVector != Vector3.zero);
+    }
+
+    private void CharacterRotate()
+    {
+        if (_InputVector == Vector3.zero) return;
+
+        characterTr.rotation = Quaternion.Lerp(characterTr.rotation,
+            Quaternion.LookRotation(new Vector3(_InputVector.x, 0, _InputVector.z)), rotateSpeed);
+    }
+}
